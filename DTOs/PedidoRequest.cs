d0eb44bf15baca7470d184bfaa0a4deebeef3491
Fabián Garrido <lusoftware100@gmail.com>
2026-02-22 00:50:00 -0500

    Ajustes finales, validaciones y manejo de excepciones

---
+++ DTOs/PedidoRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PedidosAPI.DTOs
+{
+    public class PedidoRequest
+    {
+        public int ClienteId { get; set; }
+        public string Usuario { get; set; }
+        public List<ItemRequest> Items { get; set; }
+    }
+
+    public class ItemRequest
+    {
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
