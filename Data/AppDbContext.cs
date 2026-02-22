8e8604e5ebfa1f08e7fdb70bfbd97ed253cb6ba6
Fabián Garrido <lusoftware100@gmail.com>
2026-02-21 23:40:00 -0500

    Configuración del contexto de datos y conexión a SQL Server

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PedidosAPI.Models;
+
+namespace PedidosAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PedidoCabecera> PedidoCabeceras { get; set; }
+        public DbSet<PedidoDetalle> PedidoDetalles { get; set; }
+        public DbSet<LogAuditoria> LogAuditorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PedidoDetalle>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<PedidoDetalle>()
+                .Property(p => p.PedidoId)
+                .HasColumnName("PedidoId");
+        }
+    }
+}
